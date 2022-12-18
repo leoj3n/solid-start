344f172fc728179f345b40bea312eff090904d17
leoj3n <leoj3n@gmail.com>
2022-12-18 02:02:00 -0600

    formatting/visual changes

--- components/ScrollIndicator.tsx
+++ components/ScrollIndicator.tsx
@@ -7,40 +7,45 @@ import { useLocation } from "solid-start";
 export default function ScrollIndicator(props) {
   const location = useLocation();
 
+  // we try setting this further down in the create effect
   let el: HTMLElement | undefined;
   const visible = createVisibilityObserver({ threshold: 0.8 })(() => {
-    console.log('inside observer');
+    console.log("inside observer");
     return el;
   });
 
   if (!isServer) {
     // triggers update if nav element changes width/height
-    const size = createElementSize(document.querySelector(".scroll-indicator-inner").parentElement.parentElement as HTMLDivElement);
+    const size = createElementSize(
+      document.querySelector(".scroll-indicator-inner").parentElement.parentElement as HTMLElement
+    );
 
     createEffect(() => {
+      // triggers update on location change
+      location.pathname;
+
       const activeLinkEl = document.querySelector(".scroll-indicator-inner a.text-primary") as HTMLElement;
 
       // Q: This doesn't work (visible is never true)
+      // Probably need to use ref instead to get active link or pass in ref via props?
       el = activeLinkEl;
 
-      console.log('visible?', visible(), activeLinkEl)
+      console.log("visible?", visible(), activeLinkEl);
 
       if (activeLinkEl !== null) {
         const scrollHeight = document.querySelector(".scroll-indicator-inner")?.scrollHeight || 0;
 
         const scrollMarkerEl = document.querySelector(".scroll-marker-container span") as HTMLElement;
         scrollMarkerEl.style.top = Math.ceil((activeLinkEl.offsetTop * size.height) / scrollHeight) + "px";
-        // scrollMarkerEl.style.display = visible() ? "block" : "none";
       }
-
-      // triggers update on location change
-      console.log('Setting scroll indicator for', location.pathname);
     });
   }
 
   return (
     <div class="scroll-indicator-inner px-8 py-8">
-      <div class="scroll-marker-container"><span classList={{ 'hidden': visible() }}></span></div>
+      <div class="scroll-marker-container">
+        <span classList={{ hidden: visible() }}></span>
+      </div>
       {props.children}
     </div>
   );
